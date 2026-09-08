548427d5a2bdd45c8b896892457b367a4650e1bd
seungpill83-sudo <seungpill83@gmail.com>
2026-09-08 22:09:48 +0900

    Add 내차일지 carlog sw.js

---
+++ carlog/sw.js
@@ -0,0 +1,32 @@
+const CACHE = "naecha-ilji-v1";
+const ASSETS = ["./", "./index.html", "./manifest.json", "./icon.svg"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  const req = e.request;
+  if (req.method !== "GET" || !req.url.startsWith("http")) return;
+  e.respondWith(
+    caches.match(req).then(
+      (cached) =>
+        cached ||
+        fetch(req)
+          .then((res) => {
+            const copy = res.clone();
+            caches.open(CACHE).then((c) => c.put(req, copy)).catch(() => {});
+            return res;
+          })
+          .catch(() => cached)
+    )
+  );
+});
